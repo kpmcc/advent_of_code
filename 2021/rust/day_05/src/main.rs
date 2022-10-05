20884af46393f6e335e1d6f2e6cf84bf68bc01df
Kyle McCornack <kpmcc@fastmail.com>
2022-10-05 15:51:35 -0500

    day 5 pt 2

--- 2021/rust/day_05/src/main.rs
+++ 2021/rust/day_05/src/main.rs
@@ -4,12 +4,16 @@ use std::path::Path;
 const GRID_SIZE: usize = 1000;
 
 struct Coord {
-    x: u16,
-    y: u16,
+    x: i16,
+    y: i16,
 }
 
 fn valid_segment(a: &Coord, b: &Coord) -> bool {
     return a.x == b.x || a.y == b.y;
+}
+
+fn valid_diagonal_segment(a: &Coord, b: &Coord) -> bool {
+    return (a.x - b.x).abs() == (a.y - b.y).abs();
 }
 
 fn parse_coord(s: String) -> Coord {
@@ -17,10 +21,10 @@ fn parse_coord(s: String) -> Coord {
     assert!(vals.len() == 2, "parse_coord: vals len not 2");
     let x = vals[0]
         .to_string()
-        .parse::<u16>()
+        .parse::<i16>()
         .expect("parse_coord: couldn't parse x into u16");
     let y = vals[1]
-        .parse::<u16>()
+        .parse::<i16>()
         .expect("parse_coord: couldn't parse y into u16");
     let c = Coord { x, y };
     return c;
@@ -86,16 +90,42 @@ fn apply_segment(grid: &mut [[u16; GRID_SIZE]; GRID_SIZE], a: Coord, b: Coord) -
     return new_danger_points;
 }
 
+fn apply_diagonal_segment(grid: &mut [[u16; GRID_SIZE]; GRID_SIZE], a: Coord, b: Coord) -> u16 {
+    let mut new_danger_points = 0;
+    let x_dir = (b.x - a.x) / (b.x - a.x).abs();
+    let y_dir = (b.y - a.y) / (b.y - a.y).abs();
+    let mut x = a.x;
+    let mut y = a.y;
+    loop {
+        let v = grid[y as usize][x as usize];
+        grid[y as usize][x as usize] = v + 1;
+        if v == 1 {
+            new_danger_points += 1;
+        }
+        if x == b.x && y == b.y {
+            break;
+        }
+        x += x_dir;
+        y += y_dir;
+    }
+    return new_danger_points;
+}
+
 fn main() {
     let file_contents = read_file("../advent_of_code/2021/input/05.txt");
     let lines: Vec<&str> = file_contents.split("\n").collect();
     let mut valid_segments: Vec<(Coord, Coord)> = Vec::new();
+    let mut valid_diag_segments: Vec<(Coord, Coord)> = Vec::new();
     let num_lines = lines.len();
     for l in lines {
         if l != "" {
             let (a, b) = parse_segment(l.to_string());
             if valid_segment(&a, &b) {
                 valid_segments.push((a, b))
+            } else {
+                if valid_diagonal_segment(&a, &b) {
+                    valid_diag_segments.push((a, b));
+                }
             }
         }
     }
@@ -110,11 +140,11 @@ fn main() {
 
     //for i in 0..GRID_SIZE {
     //    for j in 0..GRID_SIZE {
-    //        let x = grid[i][j];
-    //        if x == 0 {
+    //        let v = grid[i][j];
+    //        if v == 0 {
     //            print!(".");
     //        } else {
-    //            print!("{}", x);
+    //            print!("{}", v);
     //        }
     //    }
     //    println!("");
@@ -122,4 +152,23 @@ fn main() {
 
     println!("num lines: {}, num valid segments: {}", num_lines, l);
     println!("num danger points: {}", num_danger_points);
+
+    for d in valid_diag_segments {
+        let (a, b) = d;
+        num_danger_points += apply_diagonal_segment(&mut grid, a, b);
+    }
+
+    //for i in 0..GRID_SIZE {
+    //    for j in 0..GRID_SIZE {
+    //        let v = grid[i][j];
+    //        if v == 0 {
+    //            print!(".");
+    //        } else {
+    //            print!("{}", v);
+    //        }
+    //    }
+    //    println!("");
+    //}
+
+    println!("num danger points with diags: {}", num_danger_points);
 }
